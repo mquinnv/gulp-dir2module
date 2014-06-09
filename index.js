b4510cd4166b59fa40ecffc892e2f0c1e1ec403e
michael <michael.ventura@gmail.com>
2014-06-09 11:09:42 -0400

    preparing to publish

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+var through = require('through');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+var Buffer = require('buffer').Buffer;
+
+module.exports = function (fileName) {
+  if (!fileName) {
+    throw new PluginError('dir2module', 'Missing fileName option for dir2module');
+  }
+
+  var firstFile,
+    files = [];
+
+  function add(file) {
+    if (file.isNull()) {
+      return;
+    }
+
+    if (file.isStream()) {
+      return this.emit('error', new PluginError('dir2module', 'Streaming not supported'));
+    }
+    if (!firstFile) {
+      firstFile = file;
+    }
+    if (file.path.substring(file.base.length) !== fileName) {
+      files.push(file.path.substring(file.base.length, file.path.length - 3));
+    }
+
+  }
+
+  function endStream() {
+    if (files.length === 0) {
+      return this.emit('end');
+    }
+
+    var contents = '\'use strict\';\nmodule.exports = {\n' +
+        files.map(function (f) {
+          return '  \'' + f + '\': require(\'./' + f + '\')';
+        }).join(',\n') +
+        '\n};',
+      moduleFile = new File({
+        cwd: firstFile.cwd,
+        base: firstFile.base,
+        path: firstFile.base + '/' + fileName,
+        contents: new Buffer(contents)
+      });
+
+    this.emit('data', moduleFile);
+
+
+    this.emit('end');
+  }
+
+  return through(add, endStream);
+};
